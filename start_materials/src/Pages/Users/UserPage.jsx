8e51f404715f04cb19cc067702dd97512cba10e3
Anastasiia Polishchuk <polishchuck1818@gmail.com>
2024-03-26 18:18:48 +0200

    New update

---
+++ start_materials/src/Pages/Users/UserPage.jsx
@@ -0,0 +1,17 @@
+import { Link, useLoaderData } from 'react-router-dom';
+
+export default function UserPage() {
+    const user = useLoaderData();
+    return (
+        <div className='Main user-page'>
+            <div>
+                <Link to="/users">Back</Link>
+            </div>
+            <img src={user.img} alt='avatar'/>
+            <h2>User: {user.name}</h2>
+            <h2>Age: {user.age}</h2>
+            <h2>Email: {user.email}</h2>
+        </div>
+        
+    )
+}
---
+++ start_materials/src/Pages/Users/Users.jsx
@@ -0,0 +1,18 @@
+import {Link} from 'react-router-dom';
+
+export default function Users() {
+    return (
+        <div className="Main">
+            <h1>Users</h1>
+            <ul>
+                <li>
+                    <Link to="/users/walles">John Walles</Link>
+                </li>
+                <li>
+                    <Link to="/users/beanit">Jim Beanit</Link>
+                </li>
+            </ul>
+
+        </div>
+    )
+}
